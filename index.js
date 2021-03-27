1e9f8b4dac84c6b94b680cb0d006dac663bdbe95
coldkillerr <yonishitjain123456@gmail.com>
2021-03-27 11:18:45 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+	students=`[
+	{
+		"name": "Nishit",
+		"age" : 21,
+		"height" : "190"
+		
+	},
+
+	{
+		"name": "Jain",
+		"age" : 27,
+		"height" : "200"
+		
+	}
+
+]`
+
+console.log(JSON.parse(students));
+
+	
+
+	
+	
+
+	
+	
+
+
+
+
+
+
+	
+	
+
