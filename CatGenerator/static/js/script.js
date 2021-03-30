697b555e23f429f232d0c6f942daa9f7d68a1d74
coldkillerr <yonishitjain123456@gmail.com>
2021-03-30 11:55:08 +0530

    added catGenerator Project

---
+++ CatGenerator/static/js/script.js
@@ -0,0 +1,11 @@
+function catAdd(){
+
+    var img = document.createElement('img');
+    img.setAttribute('src', 'https://www.watchumor.com/wp-content/uploads/2016/12/Curious-cat-is-curious.gif');
+   
+    document.getElementById('flex-box-result').appendChild(img);
+    
+
+
+
+}
