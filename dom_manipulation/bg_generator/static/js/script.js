8a6ad0a56f9d5072fbee2b88d402f12019d3ad77
coldkillerr <yonishitjain123456@gmail.com>
2021-04-09 20:23:06 +0530

    added dom_manipulation

---
+++ dom_manipulation/bg_generator/static/js/script.js
@@ -0,0 +1,25 @@
+var css = document.querySelector('h3');
+var color1 = document.querySelector('#color1');
+var color2 = document.querySelector('#color2');
+var body = document.getElementById('gradient');
+css.textContent=body.style.background;
+
+color1.addEventListener('change',colorchangeh3);
+color2.addEventListener('change',colorchangeh3);
+color1.addEventListener('input',colorchangebg);
+color2.addEventListener('input',colorchangebg);
+
+
+function colorchangeh3() {
+	// body...
+	css.textContent=body.style.background;
+}
+
+
+function colorchangebg(){
+ 
+	body.style.background = "linear-gradient(to right,"+color1.value+","+color2.value+")";
+	
+	// console.log(body.style.background);
+}
+
